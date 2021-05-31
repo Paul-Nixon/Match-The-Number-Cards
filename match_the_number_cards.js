be3908af5e6d59bb8f705b8a45fc4f71cdae468f
Paul-Nixon <nixonp4@gmail.com>
2021-05-30 19:03:00 -0500

    glitch szn

--- match_the_number_cards.js
+++ match_the_number_cards.js
@@ -19,11 +19,11 @@ function ready()
     const scoreboard = 
     {
         score: 0,
-        tries: 0
+        tries: 3
     };
 
     // Add an event listener to each of the cards that'll call evaluateChoice().
-    document.querySelector(".grid-flip-card").forEach(card => {
+    document.querySelectorAll(".grid-flip-card").forEach(card => {
         card.addEventListener("click", (event) => {
             evaluateChoice(event.currentTarget, scoreboard);
         })
@@ -36,6 +36,7 @@ function ready()
 function evaluateChoice(card, scoreboard)
 {
     // Flip the chosen card.
+    card.classList.toggle("flipped");
 }
 
 /*
@@ -44,19 +45,19 @@ function evaluateChoice(card, scoreboard)
 function generateCardNumber()
 {
     // Get the cards and randomly generate a number.
-    let cards = document.querySelector(".grid-flip-card");
+    let cards = Array.from(document.querySelectorAll(".grid-flip-card"));
     let num = Math.floor(Math.random() * 11);
 
     // Initialize two cards with the number.
     let firstCard = cards.splice(Math.floor(Math.random() * 4), 1);
-    firstCard.querySelector(".flip-card-back").innerText = "" + num;
+    firstCard[0].querySelector(".flip-card-back").innerText = "" + num;
     let secondCard = cards.splice(Math.floor(Math.random() * 3), 1);
-    secondCard.querySelector(".flip-card-back").innerText = "" + num;
+    secondCard[0].querySelector(".flip-card-back").innerText = "" + num;
 
     // Initialize the remaining cards with a different number.
     cards.forEach(card => {
         num = Math.floor(Math.random() * 11);
-        while (num === Number.parseInt(firstCard.querySelector(".flip-card-back").innerText))
+        while (num === Number.parseInt(firstCard[0].querySelector(".flip-card-back").innerText))
         {
             num = Math.floor(Math.random() * 11);
         }
